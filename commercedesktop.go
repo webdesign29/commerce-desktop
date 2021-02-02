430c12c9796f3752b56d615a545ce63ac5bcd37f
BenFavre <ben@webdesign29.net>
2021-02-02 03:55:36 +0100

    update example

--- commercedesktop.go
+++ commercedesktop.go
@@ -2,41 +2,17 @@ package main
 
 import (
 	"github.com/webview/webview"
-	"log"
-	"runtime"
-	"github.com/mouuff/go-rocket-update/pkg/provider"
-	"github.com/mouuff/go-rocket-update/pkg/updater"
 )
 
 func main() {
-	u := &updater.Updater{
-		Provider: &provider.Github{
-			RepositoryURL: "github.com/webdesign29/commerce-desktop",
-			ZipName:       "binaries_" + runtime.GOOS + ".zip",
-		},
-		BinaryName: "commercedesktop",
-		Version:    "v1.0.1", // You can change this value to trigger an update
-	}
-	log.Println(u.Version)
-	err := u.Update()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	debug := true
 	w := webview.New(debug)
 	defer w.Destroy()
-	w.SetTitle("Application E-repas v1.0.1")
+	w.SetTitle("Application E-repas 1.0")
 	w.Bind("printPOS", func(obj string) string {
 		return "I am now printing stuff"
 	})
-	w.SetSize(800, 600, webview.HintNone)
+	w.SetSize(1200, 800, webview.HintNone)
 	w.Navigate("https://gestion.e-repas.com")
 	w.Run()
-}
-
-
-
-func add(a float64, b float64) float64 {
-	return a + b
 }
